6087f07c5112c5a602145f7c8358dbf24fa8a549
SooMin220 <pmes33@naver.com>
2020-06-26 18:13:47 +0900

    테이블명 수정 actuator 추가

--- hwSpringBoot/src/main/java/kr/ac/hansung/cse/model/Product.java
+++ hwSpringBoot/src/main/java/kr/ac/hansung/cse/model/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @NoArgsConstructor
+@Table(name = "productTable")
 public class Product implements Serializable{
 	
 	/**
